7332233f8f9664b410c25dbe75a313b045322382
Aboud <aboud.zakaria@gmail.com>
2018-10-25 16:42:28 +0200

    better gamma correction

--- preprocess_pngs.py
+++ preprocess_pngs.py
@@ -10,11 +10,11 @@ PNG_DIR = os.path.join(CURRENT_DIR, "PNG")
 PNG_PROCESSED_DIR = os.path.join(CURRENT_DIR, "PNG_PROCESSED")
 if not os.path.exists(PNG_PROCESSED_DIR): os.mkdir(PNG_PROCESSED_DIR)
 
-GAMMA, RGB_RANGE = 2.2, 255.0
+GAMMA, RGB_RANGE = 2.2, 4095.0
 i, n = 0, 10
 force_process = True
 
-for filename in os.listdir(PNG_DIR):
+for filename in sorted(os.listdir(PNG_DIR)):
     if filename.lower().endswith(".png"):
         if n < 0 or i < n:
             i += 1
@@ -27,12 +27,16 @@ for filename in os.listdir(PNG_DIR):
         try:
             print("processing '{0}'".format(filename))
             filepath = os.path.join(PNG_DIR, filename)
-            image = cv2.imread(filepath)
+            image = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
+
+#            maxrgb = image.max(axis=0).max(axis=0)
+#            minrgb = image.min(axis=0).min(axis=0)
+#            RGB_RANGE = (maxrgb - minrgb)
 
             # apply inverse gamma correction
             image = image / RGB_RANGE
             image = image ** (1.0/GAMMA)
-            image = image * RGB_RANGE
+            image = image * 255.0
 
             filepath = os.path.join(PNG_PROCESSED_DIR, filename)
             cv2.imwrite(filepath, image)
