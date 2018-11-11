5abd81db4b9e6cf44e193f21751bdb906d9629b8
Aboud <aboud.zakaria@gmail.com>
2018-11-11 16:38:13 +0100

    output dataset.csv

--- dataset_build.py
+++ dataset_build.py
@@ -13,6 +13,8 @@ CHECKER_DIR = os.path.join(CURRENT_DIR, "CHECKER")
 i, n = 0, -1
 BREAK_ONERROR = True
 
+dataset_csv = open('dataset.csv', 'w')
+
 print("building dataset.")
 dataset = {}
 for image_filename in sorted(os.listdir(PNG_PROCESSED_DIR)):
@@ -27,10 +29,11 @@ for image_filename in sorted(os.listdir(PNG_PROCESSED_DIR)):
         break
 
     try:
+        # output dictionary
         checkerfilename = image_filename.replace(".PNG", "_mask.txt")
-        csv = np.genfromtxt(os.path.join("CHECKER", checkerfilename), delimiter=',')
-        ROI = csv[0]
-        coords = csv[1:]
+        checker_csv = np.genfromtxt(os.path.join("CHECKER", checkerfilename), delimiter=',')
+        ROI = checker_csv[0]
+        coords = checker_csv[1:]
         polys = []
         for i in range(0, coords.shape[0], 2):
             xm = coords[i] + ROI[0]
@@ -39,6 +42,15 @@ for image_filename in sorted(os.listdir(PNG_PROCESSED_DIR)):
 
         imgdata = { "ROI": ROI, "polys": polys } #, "coords": coords }
         dataset[image_filename] = imgdata
+
+        # output csv
+        for i in range(len(polys)):
+            p = polys[i]
+            x1 = min(p[0][0], p[3][0]) # min(x1, x4)
+            y1 = min(p[0][1], p[1][1]) # min(y1, y2)
+            x2 = max(p[2][0], p[1][0]) # max(x3, x2)
+            y2 = max(p[2][1], p[3][1]) # max(y3, y4)
+            dataset_csv.write(f"{image_filename},{x1},{y1},{x2},{y2},{i}\n")
 
     except Exception as err:
         print("Exception: {0}".format(err))
