e1c25fe40c4ec0a014e008b59e565ee6214e6926
Aboud <aboud.zakaria@gmail.com>
2018-11-27 14:36:46 +0100

    use paul's gamma correction function

--- gamma_correction.py
+++ gamma_correction.py
@@ -3,7 +3,9 @@
 #
 
 import os
-import cv2
+#import cv2
+import matplotlib.pyplot as plt
+from read_image import read_image
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 PNG_DIR = os.path.join(CURRENT_DIR, "PNG")
@@ -29,19 +31,20 @@ for filename in sorted(os.listdir(PNG_DIR)):
         try:
             print("processing '{0}'".format(filename))
             filepath = os.path.join(PNG_DIR, filename)
-            image = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
+#            image = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
 
 #            maxrgb = image.max(axis=0).max(axis=0)
 #            minrgb = image.min(axis=0).min(axis=0)
 #            RGB_RANGE = (maxrgb - minrgb)
 
             # apply inverse gamma correction
-            image = image / RGB_RANGE
-            image = image ** (1.0/GAMMA)
-            image = image * 255.0
-
+#            image = image / RGB_RANGE
+#            image = image ** (1.0/GAMMA)
+#            image = image * 255.0
+            image = read_image(filepath)
             filepath = os.path.join(PNG_PROCESSED_DIR, filename)
-            cv2.imwrite(filepath, image)
+            plt.imsave(filepath, image)
+            #cv2.imwrite(filepath, image)
         except Exception as err:
             print("Exception: {0}".format(err))
             if BREAK_ONERROR:
